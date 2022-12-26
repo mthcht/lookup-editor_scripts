c750dd1e3a592c20a4db1492a9066825708c881c
mthcht <mathieuchotplassot@gmail.com>
2022-12-26 22:36:14 +0100

    Update update_lookups_from_splunk.py

--- update_lookups_from_splunk.py
+++ update_lookups_from_splunk.py
@@ -27,7 +27,7 @@ args = parser.parse_args()
 
 if (args.ask_input and args.paste_csv_data and args.csv_file) or (args.paste_csv_data and args.csv_file) or (args.ask_input and args.paste_csv_data) or (args.ask_input and args.csv_file):
     parser.print_help()
-    logging.error("Error: you cannot use these arguments at the same time, use either method -i or method -p")
+    logging.error("Error: you cannot use these arguments at the same time, use either method -i, -p or -f")
     sys.exit(1)
 if len(sys.argv)==1:
     # display help message when no args are passed.
